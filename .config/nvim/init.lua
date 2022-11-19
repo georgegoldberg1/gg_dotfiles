bf9fbf099c12034dd46cd35ac69413c4d318fc1f
georgegoldberg1 <45336261+georgegoldberg1@users.noreply.github.com>
2022-11-19 16:42:31 +0000

    feat: added highlighting to init requirements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,18 @@
 require('base')
 require('maps')
+require('highlights')
 require('plugins')
 
 local has = function(x)
-	return vim.fn.has(x) == 1
+    return vim.fn.has(x) == 1
 end
 
 local ismac = has 'macunix'
 --local is_win = has 'win32'
 
 if is_mac then
-	require('macos')
+    require('macos')
 end
 --if is_win then
 --	require('windows')
 --end
-
