84e02d0b4c4f94e109c94af114ba55c70abd95db
georgegoldberg1 <45336261+georgegoldberg1@users.noreply.github.com>
2022-12-02 11:39:33 -0800

    feat/bugfix: fixed yank to clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,7 @@ local has = function(x)
     return vim.fn.has(x) == 1
 end
 
-local ismac = has 'macunix'
+local is_mac = has 'macunix'
 --local is_win = has 'win32'
 
 if is_mac then
--- .config/nvim/lua/macos.lua
+++ .config/nvim/lua/macos.lua
@@ -1,1 +1,2 @@
 vim.opt.clipboard:append { 'unnamedplus' }
+--vim.opt.clipboard:append { 'unnamed' }
