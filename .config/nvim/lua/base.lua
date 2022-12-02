8c35149cf8c23d6d28eff800aabf349d0ce1e086
georgegoldberg1 <45336261+georgegoldberg1@users.noreply.github.com>
2022-12-02 11:47:35 -0800

    feat: nvim to use sh for shell not fish
    to increase performance in vim

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -6,7 +6,7 @@ vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
 
-vim.opt.title = tur
+vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
@@ -15,7 +15,16 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = 'zsh'
+
+--vim.opt.shell = 'zsh'
+-- If the current system shell or the `shell` option is set to /usr/bin/fish then revert to sh
+if os.getenv('SHELL') == "/usr/bin/fish" or vim.opt.shell == "/usr/bin/fish" then
+    vim.opt.shell = "/bin/sh"
+else
+    -- Else default to the system current shell.
+    vim.opt.shell = os.getenv('SHELL')
+end
+
 vim.opt.backupskip = '/tmp/*,/private/tmp/*'
 vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true
@@ -31,7 +40,6 @@ vim.opt.path:append { '**' } -- Finding files - search down into subfolders
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum' 
+vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
-
