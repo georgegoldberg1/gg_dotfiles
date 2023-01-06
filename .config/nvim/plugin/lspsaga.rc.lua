2f4720b93f9920320e413416c6323135d63ead4c
georgegoldberg1 <45336261+georgegoldberg1@users.noreply.github.com>
2023-01-06 15:31:03 +0000

    feat: lspsaga keybinds nextprev diagnostics

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -8,10 +8,12 @@ saga.init_lsp_saga {
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', '[d', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', ']d', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+-- terminal
+vim.keymap.set('n', '<space>-t', '<Cmd>Lspsaga openfloaterm<CR>', opts)
